2eba3223d774701314ab8b13475417dc2cbf99d9
CesarArzola <cesararzola@liceovvh.cl>
2026-04-15 08:13:23 -0400

    xd

---
+++ 02_Tipos_de_datos/1_Tipo_texto/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("conexión con js correcta...");
+/*
+Dato: Tipo texto(string)
+1.- Concatenación de texto con el signo +
+Podemos unir texto y variables
+*/
+const nombre = "César";
+const apellido = "Arzola";
+//Unimos ambas constantes con un texto extra.
+console.log("Hola, mi nombre es " + nombre + " " + apellido);
